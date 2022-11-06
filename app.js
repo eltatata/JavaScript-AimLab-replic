323a915c1b3dfd06bc7b6ebc50de4d38fa946383
eltatataa <davidtowo75@gmail.com>
2022-11-06 13:51:22 -0500

    crosshair updates

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const objective = document.getElementById("objective");
 // obtener el contenedor del modal
 const containerModal = document.querySelector(".container-modal");
 
-// dar estilos al objetivo
-objective.style = "position: absolute;right: 50%;top: 600px;color: white;font-size: 40px;background: rgb(0, 106, 255);width: 35px;height: 35px;border-radius: 100%;display: flex;justify-content: center;align-items: center;cursor: all-scroll;transition: all 0.3s;";
-
 // declarar la variables de tiempo y puntos
 time = 60;
 points = 0;
@@ -17,18 +14,18 @@ maxHeight = 0;
 // funcion para abrir modal con los resultados del jugador
 const openModal = (p) => {
     let rating;
-    
+
     // saber cual es el rango del jugador
     if (p < 50) {
         rating = "Junior";
-    } else if(p >= 50 && p <=70) {
+    } else if (p >= 50 && p <= 70) {
         rating = "Midle";
     } else if (p > 70) {
         rating = "Senior";
     }
 
     // html del modal
-    const htmlModal =   `<div id="modal">
+    const htmlModal = `<div id="modal">
                             <h2>Your results:</h2>
                             <p id="points-result">Your points: ${p}</p>
                             <p id="ratings">Your rating: ${rating}</p>
@@ -40,7 +37,7 @@ const openModal = (p) => {
     containerModal.innerHTML += htmlModal;
 }
 
-addEventListener("keydown", (e)=> {
+addEventListener("keydown", (e) => {
     // saber si se presiono |space| para inicializar el juego
     if (e.code == "Space") {
         // ocultar los textos de instruciones
@@ -49,7 +46,7 @@ addEventListener("keydown", (e)=> {
         // -------------------------------------------------------------------------------
         // CODIGO PARA SUMAR LOS PUNTOS
 
-        objective.addEventListener("click", ()=> {
+        objective.addEventListener("click", () => {
             points++;
             // mostrar la cantidad de puntos que tiene el jugador
             document.getElementById("points").textContent = `Points: ${points}`;
@@ -66,8 +63,8 @@ addEventListener("keydown", (e)=> {
             }, 200);
 
             // dar las cordenadas al objective
-            objective.style.marginRight  = maxWidth + "px"; 
-            objective.style.marginTop = maxHeight + "px"; 
+            objective.style.marginRight = maxWidth + "px";
+            objective.style.marginTop = maxHeight + "px";
         });
         // -------------------------------------------------------------------------------
         // FUNCION PARA RESTAR EL TIEMPO
@@ -80,7 +77,7 @@ addEventListener("keydown", (e)=> {
                 document.getElementById("time").textContent = `Time: ${time}`;
 
                 // si el tiempo finaliza:
-                if(time == 0) {
+                if (time == 0) {
                     // mostrar que se acabo el tiempo
                     document.getElementById("time").innerHTML = "<b class='end-msg'>END</b>";
 
@@ -94,15 +91,14 @@ addEventListener("keydown", (e)=> {
                     openModal(points);
 
                     // funcion de cuando se cierra el modal
-                    document.getElementById("close").addEventListener("click", ()=> {
+                    document.getElementById("close").addEventListener("click", () => {
                         containerModal.style.animation = "1s Disappear forwards";
 
                         // y cuado todo finalize despues de 1 segundo recargar la pagina
                         setTimeout(() => {
                             location.reload();
                         }, 1000);
-                    }) 
-
+                    })
                 }
             }
         }, 1000);
